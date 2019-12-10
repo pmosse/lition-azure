73bbe8a8706f44d3c815cfaea6054a63899ddd75
Patricio Mosse <patricio.mosse@gmail.com>
2019-12-10 12:54:25 +0100

    feat: sleep

--- scripts/clone_and_start_maker.sh
+++ scripts/clone_and_start_maker.sh
@@ -1,7 +1,8 @@
-docker pull litiondev/bc-lition:1.0
 sudo apt update
 sudo apt install git -y
-git clone https://gitlab.com/lition/lition-maker /home/pmosse
+git clone https://gitlab.com/lition/lition-maker /home/pmosse/lition-maker
+sleep 20
+docker pull litiondev/bc-lition:1.0
 #cd lition-maker
 #screen
 #./setup.sh
