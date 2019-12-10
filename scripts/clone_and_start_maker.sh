aed1a6f35b9879f8b336370e9cd7f53016bb472e
Patricio Mosse <patricio.mosse@gmail.com>
2019-12-10 12:44:09 +0100

    feat: git pull

--- scripts/clone_and_start_maker.sh
+++ scripts/clone_and_start_maker.sh
@@ -1,7 +1,7 @@
+docker pull litiondev/bc-lition:1.0
 sudo apt update
 sudo apt install git -y
-docker pull litiondev/bc-lition:1.0
-git clone https://gitlab.com/lition/lition-maker
-cd lition-maker
+git clone https://gitlab.com/lition/lition-maker /home/pmosse
+#cd lition-maker
 #screen
 #./setup.sh
